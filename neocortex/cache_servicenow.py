048a2569a653478ba09d31025ed0f71d178d3837
Clayton Peters <me@claytonpeters.co.uk>
2015-12-16 10:50:47 +0000

    Systems page now has clickable server names for viewing systems. View button has been removed. Resolves #25 - 'u_environment' and 'virtual' now pulled in from ServiceNow. Updates #20 - Puppet ENC support started. ENC link added to appropriate servers on page. Added Environments configuration variable.

--- neocortex/cache_servicenow.py
+++ neocortex/cache_servicenow.py
@@ -15,7 +15,7 @@ def run(helper, options):
 	# Make the request to download all CI data using the JSONv2 API (which allows
 	# the resolving of choice value to choice label using the displayvalue=true
 	# parameter)
-	r = requests.get('https://' + helper.config['SN_HOST'] + '/cmdb_ci_server.do?JSONv2&sysparm_fields=sys_id,sys_class_name,operational_status,u_number,name,short_description&displayvalue=true', auth=(helper.config['SN_USER'], helper.config['SN_PASS']), headers={'Accept': 'application/json'})
+	r = requests.get('https://' + helper.config['SN_HOST'] + '/cmdb_ci_server.do?JSONv2&sysparm_fields=sys_id,sys_class_name,operational_status,u_number,name,short_description,u_environment,virtual&displayvalue=true', auth=(helper.config['SN_USER'], helper.config['SN_PASS']), headers={'Accept': 'application/json'})
 
 	# Get the JSON returned from ServiceNow
 	data = r.json()
@@ -35,8 +35,13 @@ def run(helper, options):
 
 	# Iterate over the results
 	for row in data['records']:
+		virtual = False
+		if row['virtual'] is not None:
+			if row['virtual'] == 'true':
+				virtual = True
+
 		# Insert the information in to the database
-		curd.execute('INSERT INTO `sncache_cmdb_ci` (`sys_id`, `sys_class_name`, `name`, `operational_status`, `u_number`, `short_description`) VALUES (%s, %s, %s, %s, %s, %s)', (row['sys_id'], row['sys_class_name'], row['name'], row['operational_status'], row['u_number'], row['short_description']))
+		curd.execute('INSERT INTO `sncache_cmdb_ci` (`sys_id`, `sys_class_name`, `name`, `operational_status`, `u_number`, `short_description`, `u_environment`, `virtual`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)', (row['sys_id'], row['sys_class_name'], row['name'], row['operational_status'], row['u_number'], row['short_description'], row['u_environment'], virtual))
 
 	helper.end_event(description='Cached ' + str(len(data['records'])) + ' records')
 
