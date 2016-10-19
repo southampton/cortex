5b727e4678ebb89798061b89f18363156f6a144c
Clayton Peters <me@claytonpeters.co.uk>
2016-10-19 11:11:00 +0100

    Based on feedback: change expiration e-mail to next month (four weeks) rather than just a week

--- neocortex/vm_expire_notify.py
+++ neocortex/vm_expire_notify.py
@@ -9,17 +9,17 @@ def run(helper, options):
 	db = helper.db_connect()
 	curd = db.cursor(mysql.cursors.DictCursor)
 
-	helper.event('check_expire_count', 'Checking for systems due to expire in the next week')
-	curd.execute('SELECT * FROM `systems_info_view` WHERE `expiry_date` > NOW() AND `expiry_date` < NOW() + INTERVAL 7 DAY')
+	helper.event('check_expire_count', 'Checking for systems due to expire in the next four weeks')
+	curd.execute('SELECT * FROM `systems_info_view` WHERE `expiry_date` > NOW() AND `expiry_date` < NOW() + INTERVAL 28 DAY')
 	systems = curd.fetchall()
-	helper.end_event(description="Found " + str(len(systems)) + " system(s) which will expire in the next seven days")
+	helper.end_event(description="Found " + str(len(systems)) + " system(s) which will expire in the next four weeks")
 
 	## If no systems are due to expire
 	if len(systems) == 0:
-		helper.event('check_expire_none', 'No systems will expire within the next seven days, not sending notification e-mail',oneshot=True)	
+		helper.event('check_expire_none', 'No systems will expire within the next four weeks, not sending notification e-mail',oneshot=True)	
 		return
 
-	message = "The following systems will expire in the next seven days. When they expire Cortex will turn the virtual machines off and ensure they remain switched off. The virtual machines will not be deleted.\n\n"
+	message = "The following systems will expire in the next four weeks. When they expire Cortex will turn the virtual machines off and ensure they remain switched off. The virtual machines will not be deleted.\n\n"
 
 	## Check the power status of all the VMs
 	for system in systems:
@@ -30,7 +30,7 @@ def run(helper, options):
 		system_status = "off"
 		if vm.runtime.powerState == vim.VirtualMachine.PowerState.poweredOn:
 			system_status = "on"
-			helper.event('check_expire_found', 'The system ' + system['name'] + " (ID " + str(system['id']) + ") will expire in the next seven days",oneshot=True)
+			helper.event('check_expire_found', 'The system ' + system['name'] + " (ID " + str(system['id']) + ") will expire in the next four weeks",oneshot=True)
 
 		system_link = 'https://' + helper.config['CORTEX_DOMAIN'] + '/systems/edit/' + str(system['id'])
 		cmdb_link   = helper.config['CMDB_URL_FORMAT'] % system['cmdb_id']
@@ -48,5 +48,5 @@ CMDB ID:       %s - %s
 
 	helper.event('check_expire_mail', 'Sending warning e-mail for expired systems ')
 	for addr in helper.config['SYSTEM_EXPIRE_NOTIFY_EMAILS']:
-		helper.lib.send_email(addr, 'Systems expiration warning: ' + str(len(systems)) + ' system(s) will expire in the next 7 days', message)
+		helper.lib.send_email(addr, 'Systems expiration warning: ' + str(len(systems)) + ' system(s) will expire in the next 28 days', message)
 	helper.end_event(description="Sent warning e-mail for expired systems")
