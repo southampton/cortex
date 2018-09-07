4d0c1abf470ccd04ce7e3d5758015075a97f71c3
Jake Malley <jja.malley@gmail.com>
2018-09-07 09:04:40 +0100

    Tidied up the corpus folder slightly, moved the Puppet DB connect into its own file, so it can be used by other tasks, updated the sync ServiceNow task.

---
+++ corpus/PuppetDBConnector.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+import pypuppetdb
+
+class PuppetDBConnector:
+
+	def __init__(self, host, port, ssl_cert, ssl_key, ssl_verify):
+		self.host = host
+		self.port = port
+		self.ssl_cert = ssl_cert
+		self.ssl_key = ssl_key
+		self.ssl_verify = ssl_verify
+		self.facts = {}
+
+		# Connect
+		self.connect()
+
+	def connect(self):
+		self.db = pypuppetdb.connect(self.host, port=self.port, ssl_cert=self.ssl_cert, ssl_key=self.ssl_key, ssl_verify=self.ssl_verify)
+
+	def get_nodes(self):
+		return self.db.nodes()
+
+	def get_node(self, node_name):
+		return self.db.node(node_name)
+
+	def get_all_facts(self, node_object, cached=True):
+		"""Get facts about this node from puppet."""
+		if node_object.name in self.facts and cached:
+			return self.facts[node_object.name]
+		else:
+			facts = node_object.facts()
+			facts_dict = {}
+			if facts is not None:
+				for fact in facts:
+					facts_dict[fact.name] = fact.value
+
+				self.facts[node_object.name] = facts_dict
+
+			return facts_dict
+
+	def get_network_facts(self, node_object, cached=True):
+		"""Get network facts from puppet."""
+		facts = self.get_all_facts(node_object, cached)
+		try:
+			return facts["networking"]
+		except KeyError:
+			return {}
+
+	def get_disk_facts(self, node_object, cached=True):
+		"""Get disk facts from puppet."""
+		facts = self.get_all_facts(node_object, cached)
+		try:
+			return facts["disks"]
+		except KeyError:
+			return {}
+
+	def get_mountpoint_facts(self, node_object, cached=True):
+		"""Get mountpoint facts from puppet."""
+		facts = self.get_all_facts(node_object, cached)
+		try:
+			return facts["mountpoints"]
+		except KeyError:
+			return {}
--- neocortex/sync_puppet_servicenow.py
+++ neocortex/sync_puppet_servicenow.py
@@ -1,36 +1,42 @@
 #!/usr/bin/python
 
 import MySQLdb as mysql
-from corpus import SNPuppetConnector
+from corpus import SNPuppetConnector, PuppetDBConnector
 
 def run(helper, options):
 
 	# Connect to database.
 	db = helper.db_connect()
 
-	# Create the connector object.
-	connector = SNPuppetConnector.SNPuppetConnector(
+	# Create the PuppetDB connector object.
+	puppet_connector = PuppetDBConnector.PuppetDBConnector(
+		host = helper.config['PUPPETDB_HOST'],
+		port = helper.config['PUPPETDB_PORT'],
+		ssl_cert = helper.config['PUPPETDB_SSL_CERT'],
+		ssl_key = helper.config['PUPPETDB_SSL_KEY'],
+		ssl_verify = helper.config['PUPPETDB_SSL_VERIFY'],
+	)
+
+	# Create the ServiceNow connector object.
+	sn_connector = SNPuppetConnector.SNPuppetConnector(
 		sn_host = helper.config['SN_HOST'],
 		sn_version = 'v1',
 		sn_user = helper.config['SN_USER'],
 		sn_pass = helper.config['SN_PASS'],
-		puppet_host = helper.config['PUPPETDB_HOST'],
-		puppet_port = helper.config['PUPPETDB_PORT'],
-		puppet_ssl_cert = helper.config['PUPPETDB_SSL_CERT'],
-		puppet_ssl_key = helper.config['PUPPETDB_SSL_KEY'],
-		puppet_ssl_verify = helper.config['PUPPETDB_SSL_VERIFY'],
+		puppet_connector = puppet_connector,
 	)
+	
 
 	helper.event('puppet_nodes', 'Getting nodes from Puppet')
-	nodes = connector.get_nodes_from_puppet()
+	nodes = puppet_connector.get_nodes()
 	helper.end_event(description="Received nodes from Puppet")
 
 	for node in nodes:
 		helper.event('push_facts_to_service_now', 'Pushing facts for node ' + str(node.name) + ' to ServiceNow')
 
 		# Use the cortex database to get the node sys_id.
-		curd = db.cursor(mysql.cursors.SSDictCursor)
-		curd.execute('SELECT `name`, `cmdb_id`, `puppet_certname`, `vmware_ram` FROM `systems_info_view` WHERE `puppet_certname`="%s"' %(node.name))		
+		curd = db.cursor(mysql.cursors.DictCursor)
+		curd.execute('SELECT `name`, `cmdb_id`, `puppet_certname`, `vmware_ram` FROM `systems_info_view` WHERE `puppet_certname`=%s', (node.name,))
 		result = curd.fetchone()
 		curd.close()
 		# Ensure we have a result.
@@ -40,12 +46,14 @@ def run(helper, options):
 			# If there is no CMDB id then there would be nothing to update.
 			if cmdb_id is not None:
 				try:
-					# push the facts
-					connector.push_facts(node, cmdb_id, vmware_ram=result["vmware_ram"])
+					# Push the facts
+					sn_connector.push_facts(node, cmdb_id, vmware_ram=result["vmware_ram"]) # Specifically push the VMware ram from the Cortex DB.
 				except Exception as e:
-					helper.end_event(description='Failed to push facts for node ' + str(node.name), success=False)
+					helper.end_event(description='Failed to push facts for node ' + str(node.name) + ' Exception: ' + str(e), success=False)
 				else:
 					helper.end_event(description='Successfully pushed facts for node ' + str(node.name) + ' with CMDB ID ' + str(cmdb_id))
+			else:
+				helper.end_event(description='CMDB ID not found for node ' + str(node.name) + ' in the Cortex DB', success=False)
 
 		else:
 			# No result found - the certname of this node is not in the cortex db.
