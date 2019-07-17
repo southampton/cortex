7a28971333c1737ff233708f77b606a4242a4b1e
cristivai <vaialex20@gmail.com>
2019-07-17 15:16:22 +0100

    Created a basic layout for the new workflow and added some CSS for it

---
+++ workflows/create_service/views.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from cortex import app
+from cortex.lib.workflow import CortexWorkflow
+from cortex.lib.user import get_user_list_from_cache
+import cortex.lib.core
+import datetime
+from flask import Flask, request, session, redirect, url_for, flash, g, abort
+import MySQLdb as mysql
+import re
+from cortex.corpus import Corpus
+
+workflow = CortexWorkflow(__name__)
+workflow.add_permission('service.create', 'Create New Service')
+
+@workflow.route("create",title='Create New Service', order=20, permission="newserver", methods=['GET', 'POST'])
+def createservice():
+	# This function will build the whole service from scratch
+	
+	# existing_recipes = cortex.lib.core.get_existing_recipes()
+	# 
+	
+	if request.method == 'GET': # if request is get render the page itself
+		return workflow.render_template("create_service.html")#, existing_recipes_names)
+		## Show form
+	else: # if it is POST, then it does need validation
+		return workflow.render_template("create_service.html")# return redirect(url_for('task_status', id=task_id))
+"""
+@workflow.action("get_recipe", title="Create New Service", desc="Get the recipe for the existing service", permission="service.get_recipe")
+def get_recipe(name):
+	# Will need to add a a new table that will contain recipes maybe?
+	# You must find a way of referencing another VM by using the service name (which might be found in the task table?)
+	
+	
+	
+	# return recipe """
