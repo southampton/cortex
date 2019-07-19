491fb4ad0b66a3a55e4165ad89bf8016cbb997a9
cristivai <vaialex20@gmail.com>
2019-07-19 11:32:23 +0100

    The puppet API is now using authentication token instead of user login

--- api/endpoints/puppet.py
+++ api/endpoints/puppet.py
@@ -23,34 +23,34 @@ class Puppet(Resource):
 	
 	# User must be logged in and the csrf_check is not applied as there is no csrf token
 	# being passed
-	@api_login_required('post')
 	@app.disable_csrf_check
 	def post(self):
-		
-		# If the user does not have permission to add data to the table, raise InvalidPermission
-		if not does_user_have_permission("puppet.modules_info.add"):
-			raise InvalidPermissionException
-		
-		# Read the request data
-		outcome = request.json
-		# Get the database cursor
-		curd = g.db.cursor(mysql.cursors.DictCursor)
-        	# Turn off autocommit cause a lot of insertions are going to be used
-		curd.connection.autocommit(False)	
-		for module_list in request.json['puppet_classes']:
-			for module in module_list:
-				app.logger.debug(module['name'])
-				if "::" in module['name']:
-					module_name = module['name'].split("::")[0]
-					class_name = module['name'].split("::")[1]
-				else:
-					module_name = module['name']
-					class_name = "init"
-				if 'docstring' in module.keys():
-					for tag in module['docstring']['tags']:
-						class_parameter = tag['name']
-						description = tag['text']
-						tag_name = tag['tag_name']
-						curd.execute("INSERT INTO `puppet_modules_info` (`module_name`, `class_name`, `class_parameter`, `description`, `tag_name`) VALUES (%s, %s, %s, %s, %s)", (module_name, class_name, class_parameter, description, tag_name))
-		# commit the changes		
-		curd.connection.commit()
+		if 'X-Auth-Token' in request.headers:
+			if app.config['CORTEX_API_AUTH_TOKEN']	== request.headers['X-Auth-Token']:
+				# Read the request data
+				outcome = request.json
+				# Get the database cursor
+				curd = g.db.cursor(mysql.cursors.DictCursor)
+				# Turn off autocommit cause a lot of insertions are going to be used
+				curd.connection.autocommit(False)	
+				for module_list in request.json['puppet_classes']:
+					for module in module_list:
+						app.logger.debug(module['name'])
+						if "::" in module['name']:
+							module_name = module['name'].split("::")[0]
+							class_name = module['name'].split("::")[1]
+						else:
+							module_name = module['name']
+							class_name = "init"
+						if 'docstring' in module.keys():
+							for tag in module['docstring']['tags']:
+								class_parameter = tag['name']
+								description = tag['text']
+								tag_name = tag['tag_name']
+								curd.execute("INSERT INTO `puppet_modules_info` (`module_name`, `class_name`, `class_parameter`, `description`, `tag_name`) VALUES (%s, %s, %s, %s, %s)", (module_name, class_name, class_parameter, description, tag_name))
+				# commit the changes		
+				curd.connection.commit()
+			else:
+				raise InvalidPermissionException
+		else:
+			raise UnauthorizedException
