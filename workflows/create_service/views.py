08b43db4da0fa7b8b0ba6757df18d225c492b5e0
cristivai <vaialex20@gmail.com>
2019-07-24 16:56:20 +0100

    - Fixed a bug where pressing the delte button for a VM card would still trigger its modal  - Added front end for all the buttons  - Now the user cannot progress through the form if certain requirements aren't met  - Changed the a few 'name' attributes in order to handle them easier in the view  - Now the last step of the form has only two buttons: add VM and Submit. It's up to the user to customise the VM

--- workflows/create_service/views.py
+++ workflows/create_service/views.py
@@ -9,6 +9,7 @@ from flask import Flask, request, session, redirect, url_for, flash, g, abort
 import MySQLdb as mysql
 import re
 from cortex.corpus import Corpus
+import json
 
 workflow = CortexWorkflow(__name__)
 workflow.add_permission('service.create', 'Create New Service')
@@ -26,18 +27,18 @@ def createservice():
 
 	# Get the list of environments
 	environments = cortex.lib.core.get_cmdb_environments()
-
 	if request.method == 'GET':
 		autocomplete_users = get_user_list_from_cache()
 		return workflow.render_template("create_service.html", clusters=clusters, environments=environments, title="Create Service", default_cluster=workflow.config['SB_DEFAULT_CLUSTER'], default_env=workflow.config['SB_DEFAULT_ENV'], os_names=workflow.config['SB_OS_DISP_NAMES'], os_order=workflow.config['SB_OS_ORDER'], autocomplete_users=autocomplete_users)#, existing_recipes_names)
 	else: # if it is POST, then it does need validation
-		return workflow.render_template("create_service.html")# return redirect(url_for('task_status', id=task_id))
+		#Do something here to start the task for building the VMs
+		
+		#return workflow.render_template("create_service.html", clusters=clusters, environments=environments, title="Create Service", default_cluster=workflow.config['SB_DEFAULT_CLUSTER'], default_env=workflow.config['SB_DEFAULT_ENV'], os_names=workflow.config['SB_OS_DISP_NAMES'], os_order=workflow.config['SB_OS_ORDER'], autocomplete_users=autocomplete_users)
+		return json.dumps(request.form.to_dict())  # return redirect(url_for('task_status', id=task_id))
 """
 @workflow.action("get_recipe", title="Create New Service", desc="Get the recipe for the existing service", permission="service.get_recipe")
 def get_recipe(name):
 	# Will need to add a a new table that will contain recipes maybe?
 	# You must find a way of referencing another VM by using the service name (which might be found in the task table?)
-	
-	
 	
 	# return recipe """
