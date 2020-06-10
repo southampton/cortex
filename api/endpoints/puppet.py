bc0902e73e6e90eb374e32b92f31d729d55cb514
Jake Malley <jja.malley@gmail.com>
2020-06-10 11:45:04 +0100

    Replaced puppet_modules_info with a set of tables for storing module info.

--- api/endpoints/puppet.py
+++ api/endpoints/puppet.py
@@ -1,14 +1,12 @@
-from flask import request, session, jsonify, g
-from flask_restplus import Resource
-import math
+import datetime
 import json
-from cortex import app
-from cortex.api import api_manager, api_login_required
-from cortex.api.exceptions import InvalidPermissionException, NoResultsFoundException
 
-from cortex.lib.user import does_user_have_permission
-import cortex.lib.core
 import MySQLdb as mysql
+from flask import abort, g, request
+from flask_restplus import Resource
+
+from cortex import app
+from cortex.api import api_login_required, api_manager
 
 puppet_modules_info_namespace = api_manager.namespace('puppet', description='Puppet API')
 @puppet_modules_info_namespace.route('/modules_info')
@@ -19,28 +17,35 @@ class Puppet(Resource):
 
 	@app.disable_csrf_check
 	@api_login_required(allow_api_token=True)
+	@api_manager.doc(responses={200: "Success", 400: "Bad Request"})
 	def post(self):
-		# Read the request data
-		outcome = request.json
+		"""Handle POST request from cortex-puppet-strings"""
+
+		# Ensure the request container some valid data
+		if not request.json:
+			abort(400)
+
+		# Last Updated
+		last_updated = datetime.datetime.now()
+
 		# Get the database cursor
 		curd = g.db.cursor(mysql.cursors.DictCursor)
 		# Turn off autocommit cause a lot of insertions are going to be used
 		curd.connection.autocommit(False)
-		curd.execute("TRUNCATE TABLE `puppet_modules_info`") # clean up everything before inserting the new entries`
-		for module_list in request.json['puppet_classes']:
-			for module in module_list:
-				app.logger.debug(module['name'])
-				if "::" in module['name']:
-					module_name = module['name'].split("::")[0]
-					class_name = module['name'].split("::")[1]
-				else:
-					module_name = module['name']
-					class_name = "init"
-				if 'docstring' in module.keys():
-					for tag in module['docstring']['tags']:
-						class_parameter = tag['name']
-						description = tag['text']
-						tag_name = tag['tag_name']
-						curd.execute("INSERT INTO `puppet_modules_info` (`module_name`, `class_name`, `class_parameter`, `description`, `tag_name`) VALUES (%s, %s, %s, %s, %s)", (module_name, class_name, class_parameter, description, tag_name))
-		# commit the changes
+		curd.execute("TRUNCATE TABLE `puppet_docs_tags`;")
+
+		# Iterate through the request data and insert
+		for environment, modules in request.json.items():
+			for module_name, classes in modules.items():
+				curd.execute("INSERT INTO `puppet_modules` (`module_name`, `environment`, `last_updated`) VALUES (%s, %s, %s) ON DUPLICATE KEY UPDATE `id`=LAST_INSERT_ID(`id`), `last_updated`=%s; SELECT LAST_INSERT_ID() INTO @puppet_module_id;", (module_name, environment, last_updated, last_updated))
+				for class_name, docstring in classes.items():
+					desc = docstring["text"] if "text" in docstring else ""
+					tags = docstring["tags"] if "tags" in docstring else []
+					curd.execute("INSERT INTO `puppet_classes` (`module_id`, `class_name`, `desc`) VALUES (@puppet_module_id, %s, %s) ON DUPLICATE KEY UPDATE `id`=LAST_INSERT_ID(`id`), `desc`=%s; SELECT LAST_INSERT_ID() INTO @puppet_class_id;", (class_name, desc, desc))
+					for tag in tags:
+						curd.execute("INSERT INTO `puppet_docs_tags` (`class_id`, `tag`, `name`, `text`, `types`) VALUES (@puppet_class_id, %s, %s, %s, %s)", (tag["tag_name"], tag.get("name", ""), tag.get("text"), json.dumps(tag.get("types", []))))
+
+		# Commit the changes
 		curd.connection.commit()
+		# Return a 200
+		return {}, 200
--- bin/cortex-puppet-strings.py
+++ bin/cortex-puppet-strings.py
@@ -1,90 +1,108 @@
-#!/usr/bin/env python
+import json
+import os
+import subprocess
+import sys
+import tempfile
 
-from __future__ import print_function
-import os, sys, subprocess, json, requests, imp
+import requests
 
-# The cortex-puppet-strngs.conf file is in the same folder as our binary
-CONFIG_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cortex-puppet-strings.conf')
+# The cortex-puppet-strngs.json file is in the same folder as our binary
+CONFIG_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cortex-puppet-strings.json')
+CONFIG_KEYS = ["PUPPET_BINARY", "FIND_BINARY", "PUPPET_ENVIRONMENT_PATHS", "API_URL", "API_TOKEN", "API_SSL_VERIFY"]
 
-# Read the config
-def load_config():
-	d = imp.new_module('config')
-	d.__file__ = CONFIG_FILE
-	try:
-		with open(CONFIG_FILE) as config_file:
-			exec(compile(config_file.read(), CONFIG_FILE, 'exec'), d.__dict__)
-	except IOError as e:
-		print('Unable to load configuration file ' + e.strerror, file=sys.stderr)
-		sys.exit(1)
-	config = {}
-
-	for key in dir(d):
-		if key.isupper():
-			config[key] = getattr(d, key)
-
-	## ensure we have required config options
-	for wkey in ['PUPPET_BINARY', 'PUPPET_PATHS', 'API_TOKEN', 'API_URL', 'SSL_VERIFY']:
-		if not wkey in config.keys():
-			print('Missing configuration option: ' + wkey, file=sys.stderr)
+def load_config(config_file):
+	"""Load JSON Config"""
+	with open(config_file, "r") as fp:
+		try:
+			return json.loads(fp.read())
+		except ValueError as ex:
+			print("Error: Failed to decode config file {f}: {ex}".format(f=config_file, ex=ex))
 			sys.exit(1)
 
-	return config
+def validate_config(config, keys):
+	"""Validate JSON Config"""
+	return all(k in config for k in keys)
 
-def generate_puppet_strings_json(config):
-	modules_seen = set()
-	outcome = {'puppet_classes': []}
+def puppet_strings(config, environment_path):
+	"""Generate JSON using the Puppet strings binary"""
+	# Generate a tempfile to write the data to, this is the most reliable way to capture JSON, as puppet-strings sometimes outputs errors to stdout.
+	tmp_fd, tmp_path = tempfile.mkstemp()
 
-	# Iterate over all the paths that could contain Puppet modules
-	for current_path in config['PUPPET_PATHS']:
-		# List the contents of the current path
-		contents = os.listdir(current_path)
+	try:
+		# Run puppet-string with the resulting .pp files from find (this prevents us needing to iterate over all the directories)
+		# The command below is equivalent to the following:
+		# /bin/find /puppet/production -type f -name '*.pp' -exec /opt/puppetlabs/bin/puppet strings generate --format json --out /tmp/tempfile {} +
+		cmd = [config["FIND_BINARY"], str(environment_path), "-type", "f", "-name", "*.pp", "-exec", config["PUPPET_BINARY"], "strings", "generate", "--format", "json", "--out", tmp_path, "{}", "+"]
+		process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+		_, stderr = process.communicate()
 
-		# For each top-level item in the current path
-		for item in contents:
+		if process.returncode != 0:
+			raise RuntimeError("Error: puppet-strings returned non-zero exit code ({rc}): {stderr}".format(rc=process.returncode, stderr=stderr))
 
-			# Generate it's fully qualified paths
-			fq_path = os.path.join(current_path, item)
-			manifests_path = os.path.join(fq_path, 'manifests')
-			pp_wildcard_path = os.path.join(manifests_path, '**', '*.pp')
+		with open(tmp_path, "r") as fp:
+			file_data = fp.read()
 
-			# If it's a directory (i.e. could be a Puppet module)
-			if os.path.isdir(fq_path) and os.path.isdir(manifests_path):
-				# Don't do the same module twice
-				if item not in modules_seen:
-					# Add to the list of seen modules
-					modules_seen.add(item)
+		return json.loads(file_data) if file_data else {}
 
-					print("Running on " + pp_wildcard_path)
+	# Clean up the tempfile
+	finally:
+		os.close(tmp_fd)
+		os.remove(tmp_path)
 
-					# Run puppet strings, returning output to the "output" variable
-					process = subprocess.Popen([config['PUPPET_BINARY'], 'strings', 'generate', '--format', 'json', pp_wildcard_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-					output, output_err = process.communicate()
+	# Logically we should never end up here
+	return {}
 
-					# If unsuccessful
-					if process.returncode != 0:
-						print("Error running puppet strings. Exit code was " + str(process.returncode), file=sys.stderr)
-					else:
-						# Parse the 
-						try:
-							puppet_strings = json.loads(output)
-						except Exception as e:
-							print("Skipping " + item + " as no JSON output")
-							continue
+def generate_puppet_strings_json(config, environment_path):
+	"""Generate Puppet strings JSON for a given environment"""
 
-						output = {}
-						output['puppet_classes'] = []
-						for c in puppet_strings['puppet_classes']:
-							this_class_details = {}
-							this_class_details['name'] = c['name']
-							if 'docstring' in c and 'tags' in c['docstring']:
-								this_class_details['docstring'] = {'tags': c['docstring']['tags']}
-							output['puppet_classes'].append(this_class_details)
-							outcome['puppet_classes'].append(output['puppet_classes'])
+	puppet_strings_json = puppet_strings(config, environment_path)
 
-	return outcome
+	if not puppet_strings_json or "puppet_classes" not in puppet_strings_json:
+		return {}
+
+	environment_data = {}
+	for obj in puppet_strings_json["puppet_classes"]:
+		module_name = obj["name"].split("::", 1)[0]
+		# Check the module level dict exists
+		if module_name not in environment_data:
+			environment_data[module_name] = {}
+		# Create this class under the module
+		environment_data[module_name][obj["name"]] = obj["docstring"] if obj.get("docstring") else {}
+
+	return environment_data
 
 if __name__ == "__main__":
-	config = load_config()
-	outcome = generate_puppet_strings_json(config)
-	headers = {'Accept': 'application/json', 'Content-Type': 'application/json', 'X-Auth-Token': config['API_TOKEN']}
-	r = requests.post(config['API_URL'], headers=headers, json=outcome, verify=config['SSL_VERIFY'])
+	config = load_config(CONFIG_FILE)
+	if not validate_config(config, CONFIG_KEYS):
+		print("Error: Missing configuration keys, require: {}".format(CONFIG_KEYS.join(",")))
+		sys.exit(1)
+
+	data = {}
+	for env in config["PUPPET_ENVIRONMENT_PATHS"]:
+		try:
+			data[env["name"]] = generate_puppet_strings_json(config, env["path"])
+		except ValueError as ex:
+			print("Error: Failed to decode response from puppet-strings: {ex}".format(ex=ex))
+			sys.exit(1)
+		except RuntimeError as ex:
+			print("Error: Failed to call puppet-strings: {ex}".format(ex=ex))
+			sys.exit(1)
+
+	# POST data to Cortex
+	if not config["API_SSL_VERIFY"]:
+		import urllib3
+		urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+	r = requests.post(
+		config["API_URL"],
+		headers={'Accept': 'application/json', 'Content-Type': 'application/json', 'X-Auth-Token': config['API_TOKEN']},
+		json=data,
+		verify=config["API_SSL_VERIFY"]
+	)
+
+	try:
+		r.raise_for_status()
+	except Exception as ex:
+		print("Error: Failed to post data to Cortex: {ex}".format(ex=ex))
+	else:
+		sys.exit(0)
