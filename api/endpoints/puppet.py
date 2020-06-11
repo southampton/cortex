2494f5ac02c546b305893b825843455d29c8bef0
Jake Malley <jja.malley@gmail.com>
2020-06-11 16:17:50 +0100

    Added puppet_environments table to track created environments.

--- api/endpoints/puppet.py
+++ api/endpoints/puppet.py
@@ -35,13 +35,17 @@ class Puppet(Resource):
 		curd.execute("TRUNCATE TABLE `puppet_docs_tags`;")
 
 		# Iterate through the request data and insert
-		for environment, modules in request.json.items():
+		for environment_name, modules in request.json.items():
+			curd.execute("INSERT INTO `puppet_environments` (`short_name`, `environment_name`, `type`) VALUES (%s, %s, 0) ON DUPLICATE KEY UPDATE `id`=LAST_INSERT_ID(`id`)", (environment_name, environment_name))
+			curd.execute("SELECT LAST_INSERT_ID() INTO @puppet_environment_id")
 			for module_name, classes in modules.items():
-				curd.execute("INSERT INTO `puppet_modules` (`module_name`, `environment`, `last_updated`) VALUES (%s, %s, %s) ON DUPLICATE KEY UPDATE `id`=LAST_INSERT_ID(`id`), `last_updated`=%s; SELECT LAST_INSERT_ID() INTO @puppet_module_id;", (module_name, environment, last_updated, last_updated))
+				curd.execute("INSERT INTO `puppet_modules` (`module_name`, `environment_id`, `last_updated`) VALUES (%s, @puppet_environment_id, %s) ON DUPLICATE KEY UPDATE `id`=LAST_INSERT_ID(`id`), `last_updated`=%s", (module_name, last_updated, last_updated))
+				curd.execute("SELECT LAST_INSERT_ID() INTO @puppet_module_id")
 				for class_name, docstring in classes.items():
 					desc = docstring["text"] if "text" in docstring else ""
 					tags = docstring["tags"] if "tags" in docstring else []
-					curd.execute("INSERT INTO `puppet_classes` (`module_id`, `class_name`, `desc`) VALUES (@puppet_module_id, %s, %s) ON DUPLICATE KEY UPDATE `id`=LAST_INSERT_ID(`id`), `desc`=%s; SELECT LAST_INSERT_ID() INTO @puppet_class_id;", (class_name, desc, desc))
+					curd.execute("INSERT INTO `puppet_classes` (`module_id`, `class_name`, `desc`) VALUES (@puppet_module_id, %s, %s) ON DUPLICATE KEY UPDATE `id`=LAST_INSERT_ID(`id`), `desc`=%s", (class_name, desc, desc))
+					curd.execute("SELECT LAST_INSERT_ID() INTO @puppet_class_id")
 					for tag in tags:
 						curd.execute("INSERT INTO `puppet_docs_tags` (`class_id`, `tag`, `name`, `text`, `types`) VALUES (@puppet_class_id, %s, %s, %s, %s)", (tag["tag_name"], tag.get("name", ""), tag.get("text"), json.dumps(tag.get("types", []))))
 
