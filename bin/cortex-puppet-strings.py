a4d9b2da689e96dc4f870545859eefecf36c59e1
Jake Malley <jja.malley@gmail.com>
2020-06-12 11:34:49 +0100

    Updated cortex-puppet-strings from Puppet master.

--- bin/cortex-puppet-strings.py
+++ bin/cortex-puppet-strings.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 import subprocess
 import sys
 import tempfile
@@ -8,7 +9,7 @@ import requests
 
 # The cortex-puppet-strngs.json file is in the same folder as our binary
 CONFIG_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cortex-puppet-strings.json')
-CONFIG_KEYS = ["PUPPET_BINARY", "FIND_BINARY", "PUPPET_ENVIRONMENT_PATHS", "API_URL", "API_TOKEN", "API_SSL_VERIFY"]
+CONFIG_KEYS = ["PUPPET_BINARY", "FIND_BINARY", "PUPPET_ENVIRONMENTS_BASE", "API_URL", "API_TOKEN", "API_SSL_VERIFY"]
 
 def load_config(config_file):
 	"""Load JSON Config"""
@@ -23,7 +24,7 @@ def validate_config(config, keys):
 	"""Validate JSON Config"""
 	return all(k in config for k in keys)
 
-def puppet_strings(config, environment_path):
+def puppet_strings(config):
 	"""Generate JSON using the Puppet strings binary"""
 	# Generate a tempfile to write the data to, this is the most reliable way to capture JSON, as puppet-strings sometimes outputs errors to stdout.
 	tmp_fd, tmp_path = tempfile.mkstemp()
@@ -32,7 +33,7 @@ def puppet_strings(config, environment_path):
 		# Run puppet-string with the resulting .pp files from find (this prevents us needing to iterate over all the directories)
 		# The command below is equivalent to the following:
 		# /bin/find /puppet/production -type f -name '*.pp' -exec /opt/puppetlabs/bin/puppet strings generate --format json --out /tmp/tempfile {} +
-		cmd = [config["FIND_BINARY"], str(environment_path), "-type", "f", "-name", "*.pp", "-exec", config["PUPPET_BINARY"], "strings", "generate", "--format", "json", "--out", tmp_path, "{}", "+"]
+		cmd = [config["FIND_BINARY"], config["PUPPET_ENVIRONMENTS_BASE"], "-type", "f", "-name", "*.pp", "-exec", config["PUPPET_BINARY"], "strings", "generate", "--format", "json", "--out", tmp_path, "{}", "+"]
 		process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
 		_, stderr = process.communicate()
 
@@ -52,24 +53,32 @@ def puppet_strings(config, environment_path):
 	# Logically we should never end up here
 	return {}
 
-def generate_puppet_strings_json(config, environment_path):
+def generate_puppet_strings_json(config):
 	"""Generate Puppet strings JSON for a given environment"""
 
-	puppet_strings_json = puppet_strings(config, environment_path)
+	def extract_environment_from_path(path):
+		"""Extract environment from file path"""
+		return re.search(r"^(.+?)(?:\/.+)$", os.path.relpath(path, config["PUPPET_ENVIRONMENTS_BASE"])).group(1)
+
+	puppet_strings_json = puppet_strings(config)
 
 	if not puppet_strings_json or "puppet_classes" not in puppet_strings_json:
 		return {}
 
-	environment_data = {}
+	data = {}
 	for obj in puppet_strings_json["puppet_classes"]:
-		module_name = obj["name"].split("::", 1)[0]
+		# Check the environment level dict exists
+		environment = extract_environment_from_path(obj["file"])
+		if environment not in data:
+			data[environment] = {}
 		# Check the module level dict exists
-		if module_name not in environment_data:
-			environment_data[module_name] = {}
+		module_name = obj["name"].split("::", 1)[0]
+		if module_name not in data[environment]:
+			data[environment][module_name] = {}
 		# Create this class under the module
-		environment_data[module_name][obj["name"]] = obj["docstring"] if obj.get("docstring") else {}
+		data[environment][module_name][obj["name"]] = obj["docstring"] if obj.get("docstring") else {}
 
-	return environment_data
+	return data
 
 if __name__ == "__main__":
 	config = load_config(CONFIG_FILE)
@@ -77,16 +86,14 @@ if __name__ == "__main__":
 		print("Error: Missing configuration keys, require: {}".format(CONFIG_KEYS.join(",")))
 		sys.exit(1)
 
-	data = {}
-	for env in config["PUPPET_ENVIRONMENT_PATHS"]:
-		try:
-			data[env["name"]] = generate_puppet_strings_json(config, env["path"])
-		except ValueError as ex:
-			print("Error: Failed to decode response from puppet-strings: {ex}".format(ex=ex))
-			sys.exit(1)
-		except RuntimeError as ex:
-			print("Error: Failed to call puppet-strings: {ex}".format(ex=ex))
-			sys.exit(1)
+	try:
+		data = generate_puppet_strings_json(config)
+	except ValueError as ex:
+		print("Error: Failed to decode response from puppet-strings: {ex}".format(ex=ex))
+		sys.exit(1)
+	except RuntimeError as ex:
+		print("Error: Failed to call puppet-strings: {ex}".format(ex=ex))
+		sys.exit(1)
 
 	# POST data to Cortex
 	if not config["API_SSL_VERIFY"]:
